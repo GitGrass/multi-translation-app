1f2124662fa4f45e161643d20a4a4d380a9ca27a
watanabe-tgoikfyuo17 <tgoikfyuo17@gmail.com>
2023-01-29 14:51:02 +0900

    feat: create output part

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+// import { Route, HashRouter, Navigate, Routes } from "react-router-dom";
+import "./App.css";
+import Result from "./components/Result";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      // 一旦テキストベタ打ちで表示できるようにした
+      <>
+        <div className="App-body">
+          <Result output="output test" />
+        </div>
+      </>
+
+      // 以下のような実装イメージ
+      // <HashRouter basename="/translator">
+      //   <div className="App-body">
+      //     <Routes>
+      //       <Route exact path="/" render={() => <Navigate to="${firstViewPage}" />} />
+      //       <Route path="/${outputLang}/${inputText}" render={() => <Result output={outputText} />} />
+      //     </Routes>
+      //   </div>
+      // </HashRouter>
+    );
+  }
 }
 
 export default App;
