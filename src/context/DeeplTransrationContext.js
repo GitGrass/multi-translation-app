f5146c24304952848da6fbc98cf1b0db3c1776a6
ponmas <teknoh10322@gmail.com>
2023-02-03 16:24:58 +0900

    feature: add deepl api

--- src/context/DeeplTransrationContext.js
+++ src/context/DeeplTransrationContext.js
@@ -3,18 +3,14 @@ export const DeeplTransrationContext = createContext();
 // import { apiKey } from '../api/config';
 
 const DeeplTransrationContextProvider = (props) => {
-  const [text, setText] = useState('');
+  const [outputText, setText] = useState('');
   const [loading, setLoading] = useState(true);
-  const translate = (
-    apiKey = '04d72a78-ed6f-5c12-e002-5d4e67a4a8b7:fx',
-    language = 'JA',
-    text = 'Hello World'
-  ) => {
+  const translate = (searchText, targetLanguage) => {
     const params = new URLSearchParams({
-      auth_key: apiKey,
+      auth_key: '04d72a78-ed6f-5c12-e002-5d4e67a4a8b7:fx',
       //source_lang: languages.source,
-      target_lang: language,
-      text: text,
+      target_lang: targetLanguage,
+      text: searchText,
     });
 
     return fetch('https://api-free.deepl.com/v2/translate', {
@@ -38,10 +34,12 @@ const DeeplTransrationContextProvider = (props) => {
       });
   };
   return (
-    <DeeplTransrationContext.Provider value={{ text, loading, runSearch }}>
+    <DeeplTransrationContext.Provider
+      value={{ outputText, loading, translate }}
+    >
       {props.children}
     </DeeplTransrationContext.Provider>
   );
 };
 
-export default PhotoContextProvider;
+export default DeeplTransrationContextProvider;
