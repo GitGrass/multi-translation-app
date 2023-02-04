364e141f782e7735d626334f135fd83d4ab86fb4
Yamato Miyatake <ymt.mytk@gmail.com>
2023-02-04 15:38:19 +0900

    Update structure

--- src/components/Input.js
+++ src/components/Input.js
@@ -19,32 +19,35 @@ const Input = () =>{
         console.log(targetLanguage)
     }
     return (
-        <Form onSubmit={(e) => handleSubmit(e, searchText, targetLanguage)}>
-            <Segment stacked>
-                <Dropdown 
-                    //palceholder='select language' 
-                    options={languageOptions} 
-                    selection
-                    fluid
-                    value={targetLanguage} 
-                    onChange={((e, data)=>{console.log(data.value); setTergetLanguage(data.value);})}
-                />
-                <Form.TextArea
-                    type='text'
-                    value={searchText}
-                    onChange={(e)=>{setSearchText(e.target.value)}}
-                />
-                <Button  
-                    color="teal"
-                    fluidsize='large'       
-                    type="submit"
-                    className={`search-button ${searchText.trim() ? "active" : null}`}
-                    disabled={!searchText.trim()}
-                >
-                    Translate
-                </Button>
-            </Segment>
-        </Form>
+        <div>
+            <h2>Original text</h2>
+            <Form onSubmit={(e) => handleSubmit(e, searchText, targetLanguage)}>
+                <Segment stacked>
+                    <Dropdown 
+                        //palceholder='select language' 
+                        options={languageOptions} 
+                        selection
+                        fluid
+                        value={targetLanguage} 
+                        onChange={((e, data)=>{console.log(data.value); setTergetLanguage(data.value);})}
+                    />
+                    <Form.TextArea
+                        type='text'
+                        value={searchText}
+                        onChange={(e)=>{setSearchText(e.target.value)}}
+                    />
+                    <Button  
+                        color="teal"
+                        fluidsize='large'       
+                        type="submit"
+                        className={`search-button ${searchText.trim() ? "active" : null}`}
+                        disabled={!searchText.trim()}
+                    >
+                        Translate
+                    </Button>
+                </Segment>
+            </Form>
+        </div>
     )
 }
 
